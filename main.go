d97bb094aedfbfdbdb1ae25fe4824c720739fbff
lukekhamilton <luke@mettadata.com>
2018-10-04 23:22:54 +1000

    Added in cmd args

--- main.go
+++ main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
 func main() {
-	cmd := GetCommand()
-	fmt.Println("cmd: ", cmd)
+
+	cmd := os.Args[1:]
+	fmt.Println(cmd)
+
 	out, err := exec.Command(cmd[0], cmd[1:]...).Output()
+
+	// cmd := GetCommand()
+	// fmt.Println("cmd: ", cmd)
+	// out, err := exec.Command(cmd[0], cmd[1:]...).Output()
 	fmt.Printf("out: %s", out)
 	fmt.Println("err: ", err)
 }
 
 // GetCommand ...
-func GetCommand() []string {
-	return []string{"ifconfig", "-a"}
-}
+// func GetCommand() []string {
+// 	return []string{"ifconfig", "-a"}
+// }
